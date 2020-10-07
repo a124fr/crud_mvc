50a90a9da7a17597f9f1d023af14348984b8a458
Francisco de Assis <a124fr@gmail.com>
2020-10-07 15:36:03 -0300

    ajustando os arquivos da pasta docs

--- docs/script_bkp2_crud_mvc_07_10_2020.sql.sql
+++ docs/script_bkp2_crud_mvc_07_10_2020.sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 07/10/2020 às 18:53
+-- Tempo de geração: 07/10/2020 às 20:33
 -- Versão do servidor: 10.4.13-MariaDB
 -- Versão do PHP: 7.4.8
 
@@ -45,8 +45,11 @@ CREATE TABLE `funcionario` (
 --
 
 INSERT INTO `funcionario` (`codigo_func`, `primeiro_nome`, `nome_completo`, `email`, `cpf`, `rg`, `data_nascimento`, `foto`) VALUES
-(11, 'Marcos', 'Marcos da Silva', 'marcos2@mail.com', '001.101.001-03', '800.301', '2020-10-10', 'c7ed058eb74d65ccf1a09c1a44a559fd.jpeg'),
-(13, 'Mara', 'Mara da Silva Pereira', 'mara@mail.com', '001.002.002-02', '800.301', '1980-01-01', '56d1534f6d881d6d6596a03ea4662a5c.jpeg');
+(11, 'Marcos', 'Marcos da Silva', 'marcos2@mail.com', '745.535.908-08', '800.301', '2020-10-10', 'c7ed058eb74d65ccf1a09c1a44a559fd.jpeg'),
+(13, 'Mara', 'Mara da Silva Pereira', 'mara@mail.com', '959.219.616-80', '800.301', '1980-01-01', '56d1534f6d881d6d6596a03ea4662a5c.jpeg'),
+(14, 'Marcos', 'Marcos da Silva', 'marcos@mail.com', '571.932.362-70', '800.301', '1980-10-20', 'a9b58dc7d54338d81167750c0058f640.png'),
+(15, 'Antonia Maria', 'Antonia Maria Pereira da Silva', 'antonia@mail.com', '854.149.631-77', '800.301', '2001-12-12', '09893ea8b76e12d92a7d5d35a62d759b.jpeg'),
+(16, 'Marcos', 'Marcos da Silva', 'a124fr@gmail.com', '125.389.071-44', '800.301', '1990-01-01', '8c49121ea6f6eb88d45f536a37705d1b.png');
 
 -- --------------------------------------------------------
 
@@ -68,9 +71,14 @@ INSERT INTO `telefone` (`codigo_tel`, `tel_contato`, `Funcionario_codigo_func`) 
 (44, '(61) 9981-7286', 11),
 (45, '(61) 9981-7281', 11),
 (46, '(61) 9981-7287', 11),
-(47, '(61) 1001-7282', 11),
 (53, '(61) 9981-7270', 13),
-(54, '(61) 9981-7271', 13);
+(54, '(61) 9981-7271', 13),
+(55, '(61) 9981-7286', 14),
+(56, '(61) 9981-7287', 14),
+(57, '(61) 9981-7281', 14),
+(58, '(61) 9981-7286', 15),
+(59, '(61) 9981-7281', 15),
+(60, '(61) 9981-7286', 16);
 
 --
 -- Índices de tabelas apagadas
@@ -97,13 +105,13 @@ ALTER TABLE `telefone`
 -- AUTO_INCREMENT de tabela `funcionario`
 --
 ALTER TABLE `funcionario`
-  MODIFY `codigo_func` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `codigo_func` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `telefone`
 --
 ALTER TABLE `telefone`
-  MODIFY `codigo_tel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `codigo_tel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- Restrições para dumps de tabelas
